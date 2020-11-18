f28308fc8d13cf97e66c03cafef544283edc2a34
martafonde <marta4510@hotmail.com>
2020-11-18 21:39:03 +0100

    Agregar archivos de proyecto.

---
+++ MyTimer/MyTimer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MyTimer
+{
+    public delegate void MiTimer();
+    class MyTimer
+    {
+        static readonly private object l = new object();
+
+        public int interval;
+        private bool running = false;
+        private bool fin = false;
+        MiTimer f;
+
+        public MyTimer(MiTimer Func, int interv)
+        {
+            this.f = Func;
+            interval = interv;
+            Thread t = new Thread(loop);
+            t.Start();
+        }
+
+        public void loop()
+        {
+            while (!fin)
+            {
+                lock (l)
+                {
+                    if (!fin)
+                    {
+                        if (running)
+                        {
+                            f();
+                            Thread.Sleep(interval);
+                        }
+                        else
+                        {
+                            Monitor.Wait(l);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void run()
+        {
+            lock (l)
+            {
+                if (!fin)
+                {
+                    running = true;
+                    Monitor.Pulse(l);
+                }
+            }
+        }
+
+        public void pause()
+        {
+            lock (l)
+            {
+                if (!fin)
+                {
+                    running = false;
+                }
+            }
+        }
+
+        public bool repeat(ConsoleKey k)
+        {
+            if (k == ConsoleKey.D1)
+            {
+                fin = false;
+                return true;
+            }
+            else
+            {
+                fin = true;
+                running = false;
+                lock (l)
+                {
+                    Monitor.Pulse(l);
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ MyTimer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyTimer
+{
+    class Program
+    {
+        static int counter = 0;
+        static void increment()
+        {
+            ++counter;
+            Console.WriteLine(counter);
+        }
+        static void Main(string[] args)
+        {
+            MyTimer t = new MyTimer(increment, 1000);
+            //t.interval = 1000;
+            //string op = "";
+            do
+            {
+                Console.WriteLine("\nPress any key to start.");
+                Console.ReadKey();
+                t.run();
+                Console.WriteLine("\nPress any key to pause.");
+                Console.ReadKey();
+                t.pause();
+                Console.WriteLine("\nPress 1 to restart or another key to end.");
+                //op = Console.ReadLine();
+            } while (t.repeat(Console.ReadKey().Key));
+        }
+    }
+}
